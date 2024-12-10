77db8f11746dcbe8fbe6d8c774df3ba9be21eed7
Dina <dins.basu@gmail.com>
2024-12-10 11:49:18 +1100

    day09 part 2 was hard for me!

--- day09/index.ts
+++ day09/index.ts
@@ -22,7 +22,6 @@ async function main() {
 
     // solvePart1(data);
     solvePart2(data);
-
   } catch (error) {
     console.error(error);
   }
@@ -79,30 +78,120 @@ function solvePart1(data: string) {
   console.log(`Part 1: ${answer}`);
 }
 
-function solvePart2(data: string) {
-  const expandedData = []
 
-  let fileId = 0;
+/// 00...111...2...333.44.5555.6666.777.888899
+/// 00992111777.44.333....5555.6666.....8888..
+
+function solvePart2(data: string) {
+  const diskMap = [];
   let fileMarker = true;
-  for (const char of data) {
-    const block = new Array(parseInt(char)).fill(
-      fileMarker ? fileId.toString() : ".",
-    );
+  for (let i = 0; i < data.length; i++) {
     if (fileMarker) {
-      fileId += 1;
+      diskMap.push({
+        files: new Array(parseInt(data[i])).fill(Math.floor(i / 2)),
+        freeSpace: 0
+      });
+    } else {
+      diskMap.push({
+        files: [],
+        freeSpace: parseInt(data[i])
+      });
     }
-    if (block.length > 0) expandedData.push(block);
     fileMarker = !fileMarker;
   }
 
-  console.log(expandedData);
+  console.log("diskMap", diskMap);
 
-  // for (let i = expandedData.length - 1; i >= 1; i -= 1) {
-  //   const curr = expandedData[i];
+  for (let i = diskMap.length - 1; i >= 0; i--) {
+    if (diskMap[i].freeSpace > 0) {
+      continue;
+    }
 
-  // }
+    for (let j = 0; j < i; j++) {
+      if (diskMap[j].freeSpace >= diskMap[i].files.length) {
+        diskMap[j].files.push(...diskMap[i].files);
+        diskMap[j].freeSpace -= diskMap[i].files.length;
+        diskMap[i].freeSpace = diskMap[i].files.length;
+        diskMap[i].files = [];
+        break;
+      }
+    }
+  }
+
+  // reconstruct the disk
+  const disk = [];
+  for (let i = 0; i < diskMap.length; i++) {
+    disk.push(...diskMap[i].files);
+    if (diskMap[i].freeSpace > 0) {
+      disk.push(...new Array(diskMap[i].freeSpace).fill("."));
+    }
+  }
+
+  console.log("disk", disk);
+
+  let answer = 0;
+  for (let i = 0; i < disk.length; i++) {
+    if (disk[i] === ".") {
+      continue;
+    }
+    answer += disk[i] * i;
+  }
+  console.log(`Part 2: ${answer}`);
 
 
+  // console.log("diskMap", diskMap);
 }
+
+// function solvePart2(data: string) {
+//   const files = [];
+//   const freeSpaces = [];
+//   let fileMarker = true;
+//   for (const char of data) {
+//     if (fileMarker) {
+//       files.push(parseInt(char));
+//     } else {
+//       freeSpaces.push(parseInt(char));
+//     }
+//     fileMarker = !fileMarker;
+//   }
+
+//   console.log(files, freeSpaces);
+
+//   const blocksToMove: Record<string, Array<number>> = {};
+
+//   for (let i = files.length - 1; i >= 0; i--) {
+//     for (let j = 0; j < freeSpaces.length; j++) {
+//       if (files[i] <= freeSpaces[j]) {
+//         Array.isArray(blocksToMove[j])
+//           ? blocksToMove[j].push(...new Array(files[i]).fill(i))
+//           : blocksToMove[j] = [...new Array(files[i]).fill(i)];
+//         freeSpaces[j] -= files[i];
+//         freeSpaces[i - 1] = files[i];
+//         files[i] = 0;
+//         break;
+//       }
+//     }
+//   }
+//   console.log("f1", files);
+//   console.log("fs", freeSpaces);
+//   console.log("btm", blocksToMove);
+
+//   const zipped = [];
+
+//   for (let i = 0; i < files.length; i++) {
+//     zipped.push(...new Array(files[i]).fill(i));
+//     if (blocksToMove[i]) {
+//       zipped.push(...blocksToMove[i]);
+//     }
+//     if (freeSpaces[i] > 0) {
+//       zipped.push(...new Array(freeSpaces[i]).fill("."));
+//     }
+//   }
+
+//   console.log("data", expandData(data).join(""));
+//   console.log("zipped", zipped.join(""));
+// }
+/// 00...111...2...333.44.5555.6666.777.888899
+/// 00992111777.44.333....5555.6666.....8888..
 
 main();
