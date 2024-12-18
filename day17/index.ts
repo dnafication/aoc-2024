f6a78e9c629c663d5e75114e8fed928ad1babb79
Dina <dins.basu@gmail.com>
2024-12-18 16:41:05 +1100

    chore: i give up on day 17

--- day17/index.ts
+++ day17/index.ts
@@ -98,6 +98,81 @@ export function compiler(program: number[], registers: { [key: string]: BigInt }
   return output;
 }
 
+// Exits early if the output is not equal to the program
+export function compilerV2(program: number[], registers: { [key: string]: BigInt }) {
+  let instructionPointer = 0;
+  let output = [];
+
+  function getOperandValue(operand: number) {
+    if (operand >= 0 && operand <= 3) {
+      return BigInt(operand);
+    }
+    switch (operand) {
+      case 4:
+        return registers.A;
+      case 5:
+        return registers.B;
+      case 6:
+        return registers.C;
+      case 7:
+        return 7n;
+      default:
+        throw new Error("Invalid operand");
+    }
+  }
+
+  while (instructionPointer < program.length) {
+    const opcode = program[instructionPointer];
+    const operand = program[instructionPointer + 1];
+    const operandValue = getOperandValue(operand);
+
+
+    switch (opcode) {
+      case 0: // adv
+        registers.A = BigInt(registers.A / (2n ** operandValue));
+        break;
+      case 1: // bxl
+        registers.B = registers.B ^ BigInt(operand);
+        break;
+      case 2: // bst
+        registers.B = operandValue % 8n;
+        break;
+      case 3: // jnz
+        if (registers.A !== 0n) {
+          instructionPointer = operand;
+          continue;
+        }
+        break;
+      case 4: // bxc
+        registers.B = registers.B ^ registers.C;
+        break;
+      case 5: // out
+        output.push(operandValue % 8n);
+        break;
+      case 6: // bdv
+        registers.B = BigInt(registers.A / (2n ** operandValue));
+        break;
+      case 7: // cdv
+        registers.C = BigInt(registers.A / (2n ** operandValue));
+        break;
+
+      default:
+        throw new Error("Invalid opcode");
+    }
+
+    if (output.length > 0) {
+      for (let i = 0; i < output.length; i++) {
+        if (output[i] !== program[i]) {
+          return [];
+        }
+      }
+    }
+
+    instructionPointer += 2;
+  }
+  return output;
+}
+
 function solvePart1(
   { registers, program }: {
     registers: { [key: string]: BigInt };
@@ -116,13 +191,34 @@ export function checkIfEqual(arr1: number[], arr2: number[]) {
 }
 
 function solvePart2(program: number[]) {
-  const NumberOfWorkers = 10;
-  for (let i = 0; i < NumberOfWorkers; i++) {
-    const worker = new Worker(
-      new URL("./worker.ts", import.meta.url).href,
-      { type: "module" },
-    )
-    worker.postMessage({ start: i, step: NumberOfWorkers, max: 999999999, program });
+  //
+  // Using workers to parallelize the work
+  //
+  // const NumberOfWorkers = 10;
+  // for (let i = 0; i < NumberOfWorkers; i++) {
+  //   const worker = new Worker(
+  //     new URL("./worker.ts", import.meta.url).href,
+  //     { type: "module" },
+  //   )
+  //   worker.postMessage({ start: i, step: NumberOfWorkers, max: 999999999, program });
+  // }
+
+  for (let i = 9999999999; i < 99999999999; i++) {
+
+    if (i % 1000000 === 0) {
+      console.log("Checking", i);
+    }
+
+    const registers = {
+      A: BigInt(i),
+      B: 0n,
+      C: 0n,
+    }
+    const out = compilerV2(program, registers);
+    if (out.length === program.length && checkIfEqual(out, program)) {
+      console.log("Part 2:", i);
+      break;
+    }
   }
 
   console.log("Part 2 done",);
